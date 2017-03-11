977d5a19202a3682fa254d2bedeee6ee79fb7611
METROSTATE\36BDPA4 <iamninjas123@gmail.com>
2017-03-11 13:55:36 -0600

    no message

---
+++ assignments/howmany/code/howmany.js
@@ -0,0 +1,18 @@
+function doLevel1(){
+	alert("spaoat")
+}
+function doLevel2(){
+	alert("apoT")
+}
+function show(id){
+	var el = document.getElementById(id);
+	el.classLis.remove("hidden");
+}
+function doLevel3(){
+	alert("apoT")
+}
+function hide(id){
+	var el = document.getElementById(id);
+	el.classList.remove("hidden");
+}
+
